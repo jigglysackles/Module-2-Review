2ff5aab7304012e5b814585cfd54ca3f58ff0fd2
John Hoffman <gnosisadvent@live.com>
2026-05-26 14:34:02 -0500

    Populated list

--- Module-2-Review/Program.cs
+++ Module-2-Review/Program.cs
@@ -1,4 +1,26 @@
-﻿
+﻿using Module_2_Review;
 
+var grades = new List<double>();
 var students = new List<string>();
+
+var tony = new Student();
+tony.ID = 123;
+tony.Name = "Tony";
+
+var sam = new Student();
+sam.ID = 456;
+sam.Name = "Sam";
+
+var frank = new Student();
+frank.ID = 789;
+frank.Name = "Frank";
+
+var choc = new Student();
+choc.ID = 987;
+choc.Name = "Choc";
+
+students.Add(tony.Name);
+students.Add(sam.Name);
+students.Add(frank.Name);
+students.Add(choc.Name);
 
--- Module-2-Review/Student.cs
+++ Module-2-Review/Student.cs
@@ -5,34 +5,40 @@ public class Student
     public string Name { get; set; }
     public int ID { get; set; }
     public List<double> Grades { get; set; }
-    
-    
+
+
     //default
     public Student()
     {
     }
-    
+
     //Custom Constructor
     public Student(string name, int id)
     {
         Name = name;
         ID = id;
+        Grades = new List<double>();
     }
-    
+
     //methods
-    
-    public void double AddGrade(double grade)
+
+    public void AddGrade(double grade)
     {
+        if (grade < 0 || grade > 100)
+        {
+            throw new ArgumentException("Grade must be between 0 and 100", nameof(grade));
+        }
+
         Grades.Add(grade);
     }
-    
-    public void double[] AddGrade(double[] grades)
-    {
-        Grades.Add(grades);
-    }
-    
-    
-    
 
-    
+    public void AddGrade(params double[] grades)
+    {
+        Grades.AddRange(grades);
+    }
+
+    public double CalculateAverageGrade()
+    {
+        return (Grades.Any()) ? Grades.Average() : 0;
+    }
 }
